f26e1302a9e54243295e95b760e48c138fee9d59
Pat Merlino <pm@twilightsavant.com>
2020-02-25 16:49:34 -0800

    Prepare for first deployment to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+// API Routes here
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
